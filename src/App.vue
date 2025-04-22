1dc01167f2791533a22cb2c78869a38e173c3bd4
Bill AKPACHEME <136422889+Bill-Peniel@users.noreply.github.com>
2025-04-22 10:56:20 +0000

    Restored to 'e3dbf7add97cb4c4e8a24c3098ddf4b12db4702d'
    Replit-Restored-To: e3dbf7add97cb4c4e8a24c3098ddf4b12db4702d

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <Header v-if="!isDashboardRoute" />
-    <main class="flex-grow ml-64">
-      <router-view v-slot="{ Component }">
-        <component :is="Component" />
-      </router-view>
+    <main class="flex-grow">
+      <router-view />
     </main>
     <Footer v-if="!isDashboardRoute" />
   </div>
